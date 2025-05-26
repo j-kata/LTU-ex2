60524087f7aa45ac8397e1f8eaff7fd3b85a846a
katja tsareva <katja.tcareva@gmail.com>
2025-05-26 09:39:46 +0200

    add program loop template

--- LTU2/Program.cs
+++ LTU2/Program.cs
@@ -1,9 +1,35 @@
 ﻿namespace LTU2;
 
-class Program
+internal class Program
 {
+    private static bool IsRunning { get; set; } = true;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (IsRunning)
+        {
+            PrintMainMenu();
+            var input = Console.ReadLine()?.Trim();
+            if (input != null) ChooseAction(input);
+        }
+    }
+
+    static void PrintMainMenu()
+    {
+        Console.WriteLine("You’ve reached the main menu. Navigate by entering numbers to test different features.");
+
+    }
+
+
+    static void ChooseAction(string num)
+    {
+        switch (num)
+        {
+            case "0":
+                IsRunning = false;
+                break;
+            default:
+                Console.WriteLine("Invalid input. Try again");
+                break;
+        }
     }
 }
